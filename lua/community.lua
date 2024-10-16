01776994ef209e269ce91dc3c3260122a1f248c6
elankath <tarun.ramakrishna.elankath@sap.com>
2024-10-16 23:59:57 +0530

    added several pack plugins

--- lua/community.lua
+++ lua/community.lua
@@ -9,4 +9,13 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.html-cssgo" },
+  { import = "astrocommunity.pack.typst" },
+  { import = "astrocommunity.pack.programming-language-support.csv-vim" },
+  { import = "astrocommunity.pack.programming-language-support.rest-nvim" },
 }
