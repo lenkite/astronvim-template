30c6f97994f9e0bfa9345bb7944e10d436b21b2c
elankath <tarun.ramakrishna.elankath@sap.com>
2025-01-02 13:09:43 +0530

    nvim cmp buffer source

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
 
@@ -80,6 +78,42 @@ return {
         -- disable for .vim files, but it work for another filetypes
         Rule("a", "a", "-vim")
       )
+    end,
+  },
+
+  { -- override nvim-cmp plugin
+    "hrsh7th/nvim-cmp",
+    -- override the options table that is used in the `require("cmp").setup()` call
+    opts = function(_, opts)
+      -- opts parameter is the default options table
+      -- the function is lazy loaded so cmp is able to be required
+      local cmp = require "cmp"
+      -- modify the sources part of the options table
+      -- opts.sources = cmp.config.sources {
+      --   { name = "nvim_lsp", priority = 1000 },
+      --   { name = "luasnip", priority = 750 },
+      --   {
+      --     name = "buffer",
+      --     priority = 500,
+      --     option = {
+      --       get_bufnrs = function() return vim.api.nvim_list_bufs() end,
+      --     },
+      --   },
+      --   { name = "path", priority = 250 },
+      -- }
+      opts.sources = cmp.config.sources {
+        { name = "nvim_lsp", priority = 1000 },
+        { name = "luasnip", priority = 750 },
+        {
+          name = "buffer",
+          priority = 500,
+          option = {
+            get_bufnrs = function() return vim.api.nvim_list_bufs() end,
+          },
+        },
+        { name = "path", priority = 250 },
+        { name = "emoji", priority = 700 }, -- add new source
+      }
     end,
   },
 }
